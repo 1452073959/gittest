e3e1cbc6a406604e16a67c628a679d1fa22aa58e
tudin <1452073959@qq.com>
2023-08-11 16:48:42 +0800

    1

--- vuetest2/shop/src/store/index.js
+++ vuetest2/shop/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import cart from './modules/cart'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -22,6 +23,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    user
+    user,
+    cart
   }
 })
